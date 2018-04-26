a50f9f2ebbf4b020a81555d415719c538dce0e8d
Mark Battersby <mark.alfred.battersby@gmail.com>
2018-04-25 22:37:30 -0400

    Add rule

---
+++ tests/lib/rules/no-constructor-bind.js
@@ -0,0 +1,51 @@
+/**
+ * @fileoverview Use class arrow methods instead of binding in the constructor
+ * @author Mark Battersby
+ */
+'use strict'
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require('../../../lib/rules/no-constructor-bind'),
+  RuleTester = require('eslint').RuleTester
+
+RuleTester.setDefaultConfig({ parser: 'babel-eslint' })
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var error = {
+  type: 'AssignmentExpression',
+  message: 'Use arrow methods instead of bind.'
+}
+
+var ruleTester = new RuleTester()
+ruleTester.run('no-constructor-bind', rule, {
+  valid: [
+    `class myClass {
+      myFunction = () => {}
+    }`,
+
+    `class myClass {
+      constructor() {
+        this.myFunction = this.myFunction.bind({ foo: "bar" })
+      }
+    }`
+  ],
+
+  invalid: [
+    {
+      code: `
+        class myClass {
+          constructor() {
+            this.myFunction = this.myFunction.bind(this)
+          }
+        }
+      `,
+      errors: [error]
+    }
+  ]
+})
